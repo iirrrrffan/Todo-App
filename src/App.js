2daf3945c2f157b22f3832546fbe2f16a3a0b201
iirrrrffan <irfanali376246@gmail.com>
2023-10-24 18:46:34 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import  React,{ useState } from "react";
 import './App.css';
 
 function App() {
+  const [todos,setTodos]=useState([]);
+  const [todo,setTodo]=useState('');
+  const addItem=()=>{
+    setTodos([...todos,todo]);
+    setTodo('');
+  }
+  const remove=(index)=>{
+    const newTask=[...todos]
+    newTask.splice(index,1)
+    setTodos(newTask)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="header"><h1>Todo App</h1></div>
+      <div className="inputSection">
+        <input value={todo} onChange={(e)=>setTodo(e.target.value)} type='text' placeholder="add text....."/>
+        <button className="btnAdd"  onClick={addItem}>Add</button>
+      </div>
+      
+      {todos.map((value) => (
+        <div className="deleteSection">
+          <span>{value}</span>
+          <button className="btnRemove" onClick={remove}>Delete</button>
+        </div>
+      ))}
     </div>
+    
   );
 }
 
-export default App;
+export default App
